4213cb00379a995ded4b66a09a7ad93ce5fe8ada
user name <esraaodat123123@gmail.com>
2024-09-09 01:34:48 -0700

    project

---
+++ E-commerce-main (1)/E-commerce-main/E-Commerce/Back-End/E-Commerce/E-Commerce/Dto_Esraa/ProductRequestDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace E_Commerce.Dto_Esraa
+{
+    public class ProductRequestDTO
+    {
+
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public IFormFile Image { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public string? Color { get; set; }
+
+        public int? FlowerColorId { get; set; }
+
+        public decimal? PriceWithDiscount { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- E-commerce-main (1)/E-commerce-main/E-Commerce/Back-End/E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
+++ E-commerce-main (1)/E-commerce-main/E-Commerce/Back-End/E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2588439e077526cbf823faa316fd3c18b6f52d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
